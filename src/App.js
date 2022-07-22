1d4fa028ca4b3e938d1858b91d543c807d4555bd
Tyler Mayfield <tylermayfield@pop-os.localdomain>
2022-07-22 14:44:53 -0700

    I dunno

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
+import EditExercise from "./components/edit-exercises.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
@@ -11,12 +11,14 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <br/>
-      <Route path="/" exact compenent={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <Routes>
+        <Route path="/" exact compenent={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </Routes>
     </Router>
+    
   );
 }
 
