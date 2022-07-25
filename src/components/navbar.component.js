7c0488ddf85a89054a81d355942f6e3af3683139
Tyler Mayfield <tylermayfield@pop-os.localdomain>
2022-07-25 13:29:33 -0700

    adjusted bootstrap so nav links would show on my small screen

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,7 +6,8 @@ export default class Navbar extends Component {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expalnd-lg">
                 <Link to="/" className="navbar-brand">Exercise Tracker</Link>
-                <div className="collapse navbar-collapse">
+                {/* <div className="collapse navbar-collapse"></div> */}
+                <div className="">
                     <ul className="navbar-nav ar-auto">
                         <li className="navbar-item">
                             <Link to="/" className="nav-link">Excercises</Link>
